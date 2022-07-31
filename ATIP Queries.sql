0046c04070eafd63a7c6bff294da352f6ff6a097
Emmanuel787B <107367681+Emmanuel787B@users.noreply.github.com>
2022-07-30 20:55:23 -0500

    Update ATIP Queries.sql

--- ATIP Queries.sql
+++ ATIP Queries.sql
@@ -16,7 +16,7 @@ CREATE TABLE Real_Time_Traffic_Incident_Reports_Cleaned (
 
 --Inserting Data-- 
 COPY Real_Time_Traffic_Incident_Reports_Cleaned
-FROM 'C:\MYDIRECTORY\Real_Time_Traffic_Incident_Reports_Cleaned.csv'
+FROM 'C:\Users\emman\OneDrive\Documents\Data Sets\Austin Traffic Incident Project\Real_Time_Traffic_Incident_Reports_Cleaned.csv'
 WITH (FORMAT CSV, HEADER );
 
 -----------------
@@ -37,15 +37,16 @@ WHERE issue_reported ILIKE ANY(ARRAY['%collision%', '%fatal%', '%crash%']);
 
 ----------------
 
---Creating collision_date_table-- 
+--Creating collision_time_date_table-- 
 
-CREATE TABLE collision_date_table(
+CREATE TABLE collision_time_date_table(
+	time time, 
 	date date,
 	issue_reported text
 	);
 
-INSERT INTO collision_date_table
-SELECT date, issue_reported
+INSERT INTO collision_time_date_table
+SELECT time, date, issue_reported
 FROM Real_Time_Traffic_Incident_Reports_Cleaned
 WHERE issue_reported ILIKE ANY(ARRAY['%collision%', '%fatal%', '%urgent%']);
 
@@ -58,7 +59,7 @@ WHERE issue_reported ILIKE ANY(ARRAY['%collision%', '%fatal%', '%urgent%']);
 SELECT COUNT(*)
 FROM(
 SELECT *
-FROM collision_date_table
+FROM collision_time_date_table
 WHERE date BETWEEN '2018/12/21' AND '2019/03/19'
 OR date BETWEEN '2019/12/21' AND '2020/03/19'
 OR date BETWEEN '2020/12/21' AND '2021/03/19'
@@ -70,19 +71,19 @@ OR date BETWEEN '2021/12/21' AND '2022/03/19'
 SELECT COUNT(*)
 FROM(
 SELECT *
-FROM collision_date_table
+FROM collision_time_date_table
 WHERE date BETWEEN '2018/03/20' AND '2018/06/20'
 OR date BETWEEN '2019/03/20' AND '2019/06/20'
 OR date BETWEEN '2020/03/20' AND '2020/06/20'
 OR date BETWEEN '2021/03/20' AND '2021/06/20'
 ) AS subquery;
---21371
+--21371--
 
 --Summer--
 SELECT COUNT(*)
 FROM(
 SELECT *
-FROM collision_date_table
+FROM collision_time_date_table
 WHERE date BETWEEN '2018/06/21' AND '2018/09/21'
 OR date BETWEEN '2019/06/21' AND '2019/09/21'
 OR date BETWEEN '2020/06/21' AND '2020/09/21'
@@ -94,7 +95,7 @@ OR date BETWEEN '2021/06/21' AND '2021/09/21'
 SELECT COUNT(*)
 FROM(
 SELECT *
-FROM collision_date_table
+FROM collision_time_date_table
 WHERE date BETWEEN '2018/09/22' AND '2018/12/20'
 OR date BETWEEN '2019/09/22' AND '2019/12/20'
 OR date BETWEEN '2020/09/22' AND '2020/12/20'
@@ -106,26 +107,12 @@ OR date BETWEEN '2021/09/22' AND '2021/12/20'
 
 --------------------
 
---Creating collison_time_table-- 
-
-CREATE TABLE collision_time_table(
-	time time,
-	issue_reported text
-	);
-	
-INSERT INTO collision_time_table
-SELECT time, issue_reported
-FROM Real_Time_Traffic_Incident_Reports_Cleaned
-WHERE issue_reported ILIKE ANY(ARRAY['%collision%', '%fatal%', '%urgent%']);
-	
------------------------
-
 --Finding Most Dangerous Time To Drive--
 
 SELECT COUNT(*)
 FROM(
 SELECT * 
-FROM collision_time_table
+FROM collision_time_date_table
 WHERE time BETWEEN '08:00' AND '11:59'
 ORDER BY 1 
 ) AS subquery;
@@ -134,7 +121,7 @@ ORDER BY 1
 SELECT COUNT(*)
 FROM(
 SELECT * 
-FROM collision_time_table
+FROM collision_time_date_table
 WHERE time BETWEEN '12:00' AND '15:59'
 ORDER BY 1
 ) AS subquery;
@@ -143,7 +130,7 @@ ORDER BY 1
 SELECT COUNT(*)
 FROM(
 SELECT * 
-FROM collision_time_table
+FROM collision_time_date_table
 WHERE time BETWEEN '16:00' AND '19:59'
 ORDER BY 1 
 ) AS subquery;
@@ -152,7 +139,7 @@ ORDER BY 1
 SELECT COUNT(*)
 FROM(
 SELECT * 
-FROM collision_time_table
+FROM collision_time_date_table
 WHERE time BETWEEN '20:00' AND '23:59'
 ORDER BY 1
 ) AS subquery;
@@ -161,7 +148,7 @@ ORDER BY 1
 SELECT COUNT(*)
 FROM(
 SELECT * 
-FROM collision_time_table
+FROM collision_time_date_table
 WHERE time BETWEEN '00:00' AND '03:59'
 ORDER BY 1 
 ) AS subquery;
@@ -170,7 +157,7 @@ ORDER BY 1
 SELECT COUNT(*)
 FROM(
 SELECT * 
-FROM collision_time_table
+FROM collision_time_date_table
 WHERE time BETWEEN '04:00' AND '07:59'
 ORDER BY 1
 ) AS subquery;
